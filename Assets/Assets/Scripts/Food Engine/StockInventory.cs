37558487f59c3bd8c5eeb1a5ed730e5d2c5655b2
AyushSingh8x <123333848+DigiFoc@users.noreply.github.com>
2023-02-01 02:59:12 +0530

    UI Changes

--- Assets/Assets/Scripts/Food Engine/StockInventory.cs
+++ Assets/Assets/Scripts/Food Engine/StockInventory.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class StockInventory : MonoBehaviour
 {
@@ -10,13 +11,21 @@ public class StockInventory : MonoBehaviour
     public class IngredientStocks
     {
         public int potato = 1;
-        
+        public TMP_Text PotatoText;
         public int flour = 1;
+        public TMP_Text FlourText;
         public int spice = 1;
+        public TMP_Text SpiceText;
         public int oil = 1;
+        public TMP_Text OilText;
         public int besan = 1;
+        public TMP_Text BesanText;
         public int sugar = 1;
+        public TMP_Text SugarText;
         public int milk = 1;
+        public TMP_Text MilkText;
+        public int tealeaves = 1;
+        public TMP_Text TeaLeavesText;
     }
     [SerializeField]
 
@@ -26,13 +35,10 @@ public class StockInventory : MonoBehaviour
     public class FoodStock
     {
         public int samosa = 1;
-        public GameObject samosaUI;
         public int paneerTikka = 1;
-        public GameObject pannerTikkaUI;
         public int tea = 1;
-        public GameObject teaUI;
         public int pakora = 1;
-        public GameObject pakoraUI;
+        public TMP_Text FoodItemsText;
     }
     [SerializeField]
 
@@ -55,7 +61,8 @@ public class StockInventory : MonoBehaviour
             Instance = this;
         }
 
-        UpdateUI();
+        UpdateFoodStockUI();
+        UpdateIngredientStockUI();
     }
 
 
@@ -64,12 +71,20 @@ public class StockInventory : MonoBehaviour
         
     }
 
-    public void UpdateUI()
+    public void UpdateFoodStockUI()
     {
-        currentFoodStocks.samosaUI.GetComponent<Text>().text = currentFoodStocks.samosa.ToString();
-        currentFoodStocks.pakoraUI.GetComponent<Text>().text = currentFoodStocks.pakora.ToString();
-        currentFoodStocks.teaUI.GetComponent<Text>().text = currentFoodStocks.tea.ToString();
-        currentFoodStocks.pannerTikkaUI.GetComponent<Text>().text = currentFoodStocks.paneerTikka.ToString();
-
+        currentFoodStocks.FoodItemsText.text = "<sprite=8>" + currentFoodStocks.samosa.ToString() + "  <sprite=10>" + currentFoodStocks.pakora.ToString() + "  <sprite=11>" + currentFoodStocks.tea.ToString() + "  <sprite=9>" + currentFoodStocks.paneerTikka.ToString();
     }
+    public void UpdateIngredientStockUI()
+    {
+        currentIngredientStocks.PotatoText.text = "<sprite=0>" + currentIngredientStocks.potato.ToString();
+        currentIngredientStocks.FlourText.text = "<sprite=1>" + currentIngredientStocks.flour.ToString();
+        currentIngredientStocks.SpiceText.text = "<sprite=2>" + currentIngredientStocks.spice.ToString();
+        currentIngredientStocks.OilText.text = "<sprite=3>" + currentIngredientStocks.oil.ToString();
+        currentIngredientStocks.BesanText.text = "<sprite=4>" + currentIngredientStocks.besan.ToString();
+        currentIngredientStocks.SugarText.text = "<sprite=5>" + currentIngredientStocks.sugar.ToString();
+        currentIngredientStocks.MilkText.text = "<sprite=6>" + currentIngredientStocks.milk.ToString();
+        currentIngredientStocks.TeaLeavesText.text = "<sprite=7>" + currentIngredientStocks.tealeaves.ToString();
+    }
+
 }
